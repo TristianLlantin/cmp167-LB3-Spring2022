b512dd750651effe0194dafee6077cdc0c7aa92e
TristianLlantin <100375792+TristianLlantin@users.noreply.github.com>
2022-03-10 20:33:02 -0500

    Add files via upload

---
+++ Program6.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ * @author Tristian 
+ *
+ */
+public class Program6 {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		for( int i = 20; i <=150; i++) {
+			sum += i;
+		}
+		System.out.println("The sum is "+ sum);
+	}
+}
+
+//Write a for loop to calculate the sum of all the numbers between 20 and 150
+
+
+
